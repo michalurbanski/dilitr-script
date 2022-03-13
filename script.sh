6736e7fb2e28b88672e1accef6060bed12b70f62
michal_urbanski <michal_urbanski@windowslive.com>
2022-03-13 22:20:14 +0100

    Removes vnet integration for function apps

--- script.sh
+++ script.sh
@@ -11,7 +11,6 @@ RESOURCE_GROUP="$1"
 
 # locks=$(az lock list --resource-group $RESOURCE_GROUP --query "[].[id,name]" --output tsv)
 locks=$(az lock list --resource-group $RESOURCE_GROUP --query "[].id" --output tsv)
-
 for id in $locks
 do
     # echo "id: $id"
@@ -19,17 +18,24 @@ do
     echo "Deleting lock: $id"
     az lock delete --ids $id
 done
-
-echo "All locks deleted"
+echo "All locks deleted."
 
 
 webapps=$(az webapp list --resource-group $RESOURCE_GROUP --query "[].name" --output tsv)
-
 for web in $webapps
 do
     echo "Removing vnet integration for $web"
     az webapp vnet-integration remove --resource-group $RESOURCE_GROUP --name $web
     echo "Vnet integration removed for $web"
 done
+echo "All vnet integrations removed for App Services."
 
-echo "All vnet integrations removed."
+
+functionapps=$(az functionapp list --resource-group $RESOURCE_GROUP --query "[].name" --output tsv)
+for fun in $functionapps
+do
+    echo "Removing vnet integration for $fun"
+    az functionapp vnet-integration remove --resource-group $RESOURCE_GROUP --name $fun
+    echo "Vnet integration removed for $fun"
+done
+echo "All vnet integrations removed for Function Apps."
